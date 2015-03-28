5a661625cd019f87db948ae357a0f7ddca256aa9
Jeff Shaw <jeff.shaw@wda.com>
2015-03-27 20:30:42 -0400

    remove typeName from PostgreSQL's 2.10 implementation, since it's unused

--- postgresql/src/main/scala-2.10/com/wda/sdbc/postgresql/PostgreSql.scala
+++ postgresql/src/main/scala-2.10/com/wda/sdbc/postgresql/PostgreSql.scala
@@ -88,40 +88,6 @@ abstract class PostgreSql
     Select[T](queryText)
   }
 
-  def typeName(tpe: Type): String = {
-    tpe match {
-      case t if t =:= typeOf[Short] => "int2"
-      case t if t =:= typeOf[Int] => "int4"
-      case t if t =:= typeOf[Long] => "int8"
-      case t if t =:= typeOf[Float] => "float4"
-      case t if t =:= typeOf[Double] => "float8"
-      case t if t =:= typeOf[java.lang.Short] => "int2"
-      case t if t =:= typeOf[java.lang.Integer] => "int4"
-      case t if t =:= typeOf[java.lang.Long] => "int8"
-      case t if t =:= typeOf[java.lang.Float] => "float4"
-      case t if t =:= typeOf[java.lang.Double] => "float8"
-      case t if t =:= typeOf[BigDecimal] => "numeric"
-      case t if t =:= typeOf[java.math.BigDecimal] => "numeric"
-      case t if t =:= typeOf[LocalDateTime] => "timestamp"
-      case t if t =:= typeOf[OffsetDateTime] => "timestamptz"
-      case t if t =:= typeOf[Date] => "date"
-      case t if t =:= typeOf[Time] => "time"
-      case t if t =:= typeOf[java.time.LocalDate] => "date"
-      case t if t =:= typeOf[java.time.LocalTime] => "time"
-      case t if t =:= typeOf[OffsetTime] => "timetz"
-      case t if t =:= typeOf[Duration] => "interval"
-      case t if t =:= typeOf[Boolean] => "boolean"
-      case t if t =:= typeOf[java.lang.Boolean] => "boolean"
-      case t if t =:= typeOf[String] => "text"
-      case t if t =:= typeOf[scala.xml.Elem] => "xml"
-      case t if t <:< typeOf[JValue] => "json"
-      case t if t =:= typeOf[LTree] => "ltree"
-      case t if t =:= typeOf[UUID] => "uuid"
-      case t if t =:= typeOf[InetAddress] => "inet"
-      case t => throw new Exception("PostgreSQL does not understand " + t.toString)
-    }
-  }
-
   type LTree = com.wda.sdbc.postgresql.LTree
 
   val LTree = com.wda.sdbc.postgresql.LTree
