d57852052e3819ebaa0a0dc4fc9851259c3f4424
Jeff Shaw <jshaw@rocketfuel.com>
2015-07-17 14:55:38 -0400

    Fix isDefinedAt bug for parameter indexes.

--- cassandra/src/main/scala/com/wda/sdbc/cassandra/Index.scala
+++ cassandra/src/main/scala/com/wda/sdbc/cassandra/Index.scala
@@ -6,7 +6,7 @@ import com.datastax.driver.core.{Row => CRow}
 
 case class IntIndex(columnIndex: Int) extends Index {
   override def isDefinedAt(row: CRow): Boolean = {
-    row.getColumnDefinitions.size() < columnIndex
+    columnIndex < row.getColumnDefinitions.size()
   }
 
   override def apply(row: CRow): Int = {
@@ -21,7 +21,7 @@ case class StringIndex(columnLabel: String) extends Index {
 
   override def apply(row: CRow): Int = {
     row.getColumnDefinitions.getIndexOf(columnLabel) match {
-      case -1 => throw new SQLException()
+      case -1 => throw new SQLException(s"Column $columnLabel does not exist in the row.")
       case columnIndex => columnIndex
     }
   }
