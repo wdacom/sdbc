2a2c2be98da5b91e2e2222468e361fdf89e5a830
root <root@jshawd.wda.local>
2015-08-21 17:07:13 -0400

    Begin implementation of parameter method for cassandra rows.

--- cassandra/src/main/scala/com/rocketfuel/sdbc/cassandra/datastax/implementation/RowMethods.scala
+++ cassandra/src/main/scala/com/rocketfuel/sdbc/cassandra/datastax/implementation/RowMethods.scala
@@ -1,13 +1,31 @@
 package com.rocketfuel.sdbc.cassandra.datastax.implementation
 
-import com.datastax.driver.core.{Row => CRow}
+import com.datastax.driver.core.{Row => CRow, TupleValue}
 
 trait RowMethods {
 
   implicit class Row(underlying: CRow) {
 
-    def apply[T](ix: Index)(implicit getter: RowGetter[T]): Option[T] = {
+    def get[T](ix: Index)(implicit getter: RowGetter[T]): Option[T] = {
       getter(underlying, ix)
+    }
+
+    def parameter(ix: Index): Option[ParameterValue[_]] = {
+      val intIx = ix(underlying)
+      val columns = underlying.getColumnDefinitions
+      val columnType = columns.getType(intIx)
+      val columnClass = columnType.asJavaClass()
+      if (columnClass.equals(classOf[java.util.Map[_, _]])) {
+        ???
+      } else if (columnClass.equals(classOf[java.util.Set[_]])) {
+        ???
+      } else if (columnClass.equals(classOf[java.util.List[_]])) {
+        ???
+      } else if (columnClass.equals(classOf[TupleValue])) {
+        ???
+      } else {
+        ???
+      }
     }
 
   }
