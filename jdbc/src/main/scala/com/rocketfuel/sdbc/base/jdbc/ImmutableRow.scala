e97300ada05a053d16a69eb6081f6f0f6f6e6e97
Jeff Shaw <jshaw@rocketfuel.com>
2015-08-31 17:36:51 -0400

    columnIndexes should be singular for single values

--- jdbc/src/main/scala/com/rocketfuel/sdbc/base/jdbc/ImmutableRow.scala
+++ jdbc/src/main/scala/com/rocketfuel/sdbc/base/jdbc/ImmutableRow.scala
@@ -22,141 +22,141 @@ case class ImmutableRow private[jdbc] (
 
   override def wasNull: Boolean = _wasNull
 
-  private def incorrectType(): Nothing = {
-    throw new SQLException("Incorrect type")
+  private def incorrectType(typeName: String): Nothing = {
+    throw new SQLException("column does not contain a " + typeName)
   }
 
-  private def setWasNull(columnIndexes: Int): Option[Any] = {
-    val parameter = parameters(columnIndexes)
+  private def setWasNull(columnIndex: Int): Option[Any] = {
+    val parameter = parameters(columnIndex)
     _wasNull = parameter.isEmpty
     parameter
   }
 
-  override def getString(columnIndexes: Int): String = {
-    setWasNull(columnIndexes) map { case s: String => s case _ => incorrectType()} orNull
+  override def getString(columnIndex: Int): String = {
+    setWasNull(columnIndex) map { case s: String => s case _ => incorrectType("text")} orNull
   }
 
   override def getString(columnLabel: String): String = {
     getString(columnIndexes(columnLabel))
   }
 
-  override def getLong(columnIndexes: Int): Long = {
-    setWasNull(columnIndexes) map { case s: Long => s case _ => incorrectType()} getOrElse 0L
+  override def getLong(columnIndex: Int): Long = {
+    setWasNull(columnIndex) map { case s: Long => s case _ => incorrectType("bigint")} getOrElse 0L
   }
 
   override def getLong(columnLabel: String): Long = getLong(columnIndexes(columnLabel))
 
-  override def getTimestamp(columnIndexes: Int): Timestamp = {
-    setWasNull(columnIndexes) map { case s: Timestamp => s case _ => incorrectType()} orNull
+  override def getTimestamp(columnIndex: Int): Timestamp = {
+    setWasNull(columnIndex) map { case s: Timestamp => s case _ => incorrectType("timestamp")} orNull
   }
 
   override def getTimestamp(columnLabel: String): Timestamp = getTimestamp(columnIndexes(columnLabel))
 
-  override def getBinaryStream(columnIndexes: Int): InputStream = {
-    setWasNull(columnIndexes) map { case s: InputStream => s case _ => incorrectType()} orNull
+  override def getBinaryStream(columnIndex: Int): InputStream = {
+    setWasNull(columnIndex) map { case s: InputStream => s case _ => incorrectType("bytea")} orNull
   }
 
   override def getBinaryStream(columnLabel: String): InputStream = getBinaryStream(columnIndexes(columnLabel))
 
-  override def getDouble(columnIndexes: Int): Double = {
-    setWasNull(columnIndexes) map { case s: Double => s case _ => incorrectType()} getOrElse 0.0
+  override def getDouble(columnIndex: Int): Double = {
+    setWasNull(columnIndex) map { case s: Double => s case _ => incorrectType("float8")} getOrElse 0.0
   }
 
   override def getDouble(columnLabel: String): Double = getDouble(columnIndexes(columnLabel))
 
-  override def getCharacterStream(columnIndexes: Int): Reader = {
-    setWasNull(columnIndexes) map { case s: Reader => s case _ => incorrectType()} orNull
+  override def getCharacterStream(columnIndex: Int): Reader = {
+    setWasNull(columnIndex) map { case s: Reader => s case _ => incorrectType("text")} orNull
   }
 
   override def getCharacterStream(columnLabel: String): Reader = getCharacterStream(columnIndexes(columnLabel))
 
-  override def getArray(columnIndexes: Int): JdbcArray = {
-    setWasNull(columnIndexes) map { case s: JdbcArray => s case _ => incorrectType()} orNull
+  override def getArray(columnIndex: Int): JdbcArray = {
+    setWasNull(columnIndex) map { case s: JdbcArray => s case _ => incorrectType("array")} orNull
   }
 
   override def getArray(columnLabel: String): JdbcArray = getArray(columnIndexes(columnLabel))
 
-  override def getURL(columnIndexes: Int): URL = {
-    setWasNull(columnIndexes) map { case s: URL => s case _ => incorrectType()} orNull
+  override def getURL(columnIndex: Int): URL = {
+    setWasNull(columnIndex) map { case s: URL => s case _ => incorrectType("url")} orNull
   }
 
   override def getURL(columnLabel: String): URL = getURL(columnIndexes(columnLabel))
 
-  override def getBigDecimal(columnIndexes: Int): BigDecimal = {
-    setWasNull(columnIndexes) map { case s: BigDecimal => s case _ => incorrectType()} orNull
+  override def getBigDecimal(columnIndex: Int): BigDecimal = {
+    setWasNull(columnIndex) map { case s: BigDecimal => s case _ => incorrectType("numeric")} orNull
   }
 
   override def getBigDecimal(columnLabel: String): BigDecimal = getBigDecimal(columnIndexes(columnLabel))
 
-  override def getFloat(columnIndexes: Int): Float = {
-    setWasNull(columnIndexes) map { case s: Float => s case _ => incorrectType()} getOrElse 0F
+  override def getFloat(columnIndex: Int): Float = {
+    setWasNull(columnIndex) map { case s: Float => s case _ => incorrectType("float4")} getOrElse 0F
   }
 
   override def getFloat(columnLabel: String): Float = getFloat(columnIndexes(columnLabel))
 
-  override def getTime(columnIndexes: Int): Time = {
-    setWasNull(columnIndexes) map { case s: Time => s case _ => incorrectType()} orNull
+  override def getTime(columnIndex: Int): Time = {
+    setWasNull(columnIndex) map { case s: Time => s case _ => incorrectType("time")} orNull
   }
 
   override def getTime(columnLabel: String): Time = getTime(columnIndexes(columnLabel))
 
-  override def getByte(columnIndexes: Int): Byte = {
-    setWasNull(columnIndexes) map { case s: Byte => s case _ => incorrectType()} getOrElse 0
+  override def getByte(columnIndex: Int): Byte = {
+    setWasNull(columnIndex) map { case s: Byte => s case _ => incorrectType("int1")} getOrElse 0
   }
 
   override def getByte(columnLabel: String): Byte = getByte(columnIndexes(columnLabel))
 
-  override def getBoolean(columnIndexes: Int): Boolean = {
-    setWasNull(columnIndexes).contains(true)
+  override def getBoolean(columnIndex: Int): Boolean = {
+    setWasNull(columnIndex) map { case s: Boolean => s case _ => incorrectType("bool") }
   }
 
   override def getBoolean(columnLabel: String): Boolean = getBoolean(columnIndexes(columnLabel))
 
-  override def getShort(columnIndexes: Int): Short = {
-    setWasNull(columnIndexes) map { case s: Short => s case _ => incorrectType()} getOrElse 0
+  override def getShort(columnIndex: Int): Short = {
+    setWasNull(columnIndex) map { case s: Short => s case _ => incorrectType("int2")} getOrElse 0
   }
 
   override def getShort(columnLabel: String): Short = getShort(columnIndexes(columnLabel))
 
-  override def getDate(columnIndexes: Int): Date = {
-    setWasNull(columnIndexes) map { case s: Date => s case _ => incorrectType()} orNull
+  override def getDate(columnIndex: Int): Date = {
+    setWasNull(columnIndex) map { case s: Date => s case _ => incorrectType("date")} orNull
   }
 
   override def getDate(columnLabel: String): Date = getDate(columnIndexes(columnLabel))
 
-  override def getSQLXML(columnIndexes: Int): SQLXML = {
-    setWasNull(columnIndexes) map { case s: SQLXML => s case _ => incorrectType()} orNull
+  override def getSQLXML(columnIndex: Int): SQLXML = {
+    setWasNull(columnIndex) map { case s: SQLXML => s case _ => incorrectType("xml")} orNull
   }
 
   override def getSQLXML(columnLabel: String): SQLXML = getSQLXML(columnIndexes(columnLabel))
 
-  override def getInt(columnIndexes: Int): Int = {
-    setWasNull(columnIndexes) map { case s: Int => s case _ => incorrectType()} getOrElse(0)
+  override def getInt(columnIndex: Int): Int = {
+    setWasNull(columnIndex) map { case s: Int => s case _ => incorrectType("int4")} getOrElse(0)
   }
 
   override def getInt(columnLabel: String): Int = getInt(columnIndexes(columnLabel))
 
-  override def getBlob(columnIndexes: Int): Blob = {
-    setWasNull(columnIndexes) map { case s: Blob => s case _ => incorrectType()} orNull
+  override def getBlob(columnIndex: Int): Blob = {
+    setWasNull(columnIndex) map { case s: Blob => s case _ => incorrectType("bytea")} orNull
   }
 
   override def getBlob(columnLabel: String): Blob = getBlob(columnIndexes(columnLabel))
 
-  override def getBytes(columnIndexes: Int): Array[Byte] = {
-    setWasNull(columnIndexes) map { case s: Array[Byte] => s case _ => incorrectType()} orNull
+  override def getBytes(columnIndex: Int): Array[Byte] = {
+    setWasNull(columnIndex) map { case s: Array[Byte] => s case _ => incorrectType("bytea")} orNull
   }
 
   override def getBytes(columnLabel: String): Array[Byte] = getBytes(columnIndexes(columnLabel))
 
-  override def getObject(columnIndexes: Int): AnyRef = {
-    setWasNull(columnIndexes) map base.box orNull
+  override def getObject(columnIndex: Int): AnyRef = {
+    setWasNull(columnIndex) map base.box orNull
   }
 
   override def getObject(columnLabel: String): AnyRef = {
     getObject(columnIndexes(columnLabel))
   }
 
-  override def getClob(columnIndexes: Int): Clob = {
+  override def getClob(columnIndex: Int): Clob = {
     throw new UnsupportedOperationException()
   }
 
